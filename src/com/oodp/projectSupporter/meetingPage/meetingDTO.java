e251c765499acc6ca78d58fff8c33cd1179e5e0e
Yeonsuu <kimysoo215@naver.com>
2020-05-20 22:23:50 +0900

    add meetingDTO,meetingDAO

---
+++ src/com/oodp/projectSupporter/meetingPage/meetingDTO.java
@@ -0,0 +1,35 @@
+package com.oodp.projectSupporter.meetingPage;
+
+import java.sql.Date;
+
+public class meetingDTO {
+	private String when;
+	private String title;
+	private String content;
+	
+	
+	public String getWhen() {
+		return when;
+	}
+	public void setWhen(String when) {
+		this.when = when;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	@Override
+	public String toString() {
+		return "meetingDTO [when=" + when + ", title=" + title + ", content=" + content + "+ "]";
+	}
+	
+}
