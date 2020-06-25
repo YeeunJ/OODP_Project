53c1ca9f2a913412d952189a023e9e69abae04c2
YeeunJ <21800669@handong.edu>
2020-06-25 16:14:38 +0900

    add!!

--- src/com/oodp/projectSupporter/login/Button.java
+++ src/com/oodp/projectSupporter/login/Button.java
@@ -5,15 +5,26 @@ import java.sql.SQLException;
 import com.oodp.projectSupporter.dto.userDTO;
 
 public class Button {
-	private Command theCommand;
+	private LoginCommand lCommand;
+	private RegisterCommand rCommand;
 	// 생성자에서 버튼을 눌렀을 때 필요한 기능을 인지로 받는다.
 	public Button() {}
-	public Button(Command theCommand) { setCommand(theCommand); }
-	public void setCommand(Command newCommand) { this.theCommand = newCommand; }
+	public Button(LoginCommand theCommand) { setCommand(lCommand); this.rCommand = null;}
+	public Button(RegisterCommand theCommand) { setCommand(rCommand); this.lCommand = null;}
+	public void setCommand(LoginCommand newCommand) { this.lCommand = newCommand; }
+	public void setCommand(RegisterCommand newCommand) { this.rCommand = newCommand; }
 	// 버튼이 눌리면 주어진 Command의 execute 메서드를 호출한다.
-	public Boolean pressed() throws ClassNotFoundException, SQLException { return theCommand.execute();}
+	public Boolean pressed() throws ClassNotFoundException, SQLException { 
+		if( rCommand != null)return rCommand.execute();
+		else if ( lCommand != null)return lCommand.execute();
+		else
+			return false;
+			}
 	
 	public userDTO getData() {
-		return theCommand.getData();
+		if( rCommand != null)return rCommand.getData();
+		else if ( lCommand != null)return lCommand.getData();
+		else
+			return null;
 	}
 }
